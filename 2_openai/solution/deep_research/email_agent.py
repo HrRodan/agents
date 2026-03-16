60e4927d731b526b34c254d37f9563643fae9780
HrRodan <m.mueller89@gmail.com>
2026-03-16 08:31:51 +0100

    week2 day 5

--- 2_openai/solution/deep_research/email_agent.py
+++ 2_openai/solution/deep_research/email_agent.py
@@ -1,17 +1,24 @@
+from dotenv import load_dotenv
 import os
 from typing import Dict
 
-import sendgrid
-from sendgrid.helpers.mail import Email, Mail, Content, To
+#import sendgrid
+#from sendgrid.helpers.mail import Email, Mail, Content, To
 from agents import Agent, function_tool
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..","..")))
+from smtp2go_sendgrid import Mail, Email, To, Content, SendGridAPIClient
 
+load_dotenv(override=True)
+TO_MAIL = os.getenv("TO_MAIL")
+FROM_MAIL = os.getenv("FROM_MAIL")
 
 @function_tool
 def send_email(subject: str, html_body: str) -> Dict[str, str]:
     """Send an email with the given subject and HTML body"""
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
-    from_email = Email("ed@edwarddonner.com")  # put your verified sender here
-    to_email = To("ed.donner@gmail.com")  # put your recipient here
+    sg = SendGridAPIClient()
+    from_email = Email(FROM_MAIL)  # put your verified sender here
+    to_email = To(TO_MAIL)  # put your recipient here
     content = Content("text/html", html_body)
     mail = Mail(from_email, to_email, subject, content).get()
     response = sg.client.mail.send.post(request_body=mail)
